367411fb4b8f91ecf7d22c5411af08275cecdcdf
GoForIt <csmenthusiast57@gmail.com>
2025-05-07 20:38:21 +0300

    modificat cod pentru brat cu 2 motoare

--- TeamCode/src/main/java/mainCode/test.java
+++ TeamCode/src/main/java/mainCode/test.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name="TeleOP test", group="Linear OpMode")
 
 /*
-* TO-DO LIST :
+* TO-DO LIST:
 *
 * - sa modificam codul astfel incat sa respecte toate conventiile
 * */
